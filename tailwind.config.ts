9a94364411608513532d5e6b462827fffcb37ff0
Daniel Janocha <daniel.jan.janocha@gmail.com>
2023-10-22 03:48:07 +0200

    added shadcn/ui part 1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,11 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
