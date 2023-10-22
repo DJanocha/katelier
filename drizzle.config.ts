b8e9d692941259c3126f12fcffdeb4f96aa669c4
Daniel Janocha <daniel.jan.janocha@gmail.com>
2023-10-22 03:26:32 +0200

    inited project with create-t3-app

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env.mjs";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  driver: "mysql2",
+  dbCredentials: {
+    connectionString: env.DATABASE_URL,
+  },
+  tablesFilter: ["katelier-t3-pnpm_*"],
+} satisfies Config;
