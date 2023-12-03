fc76755eb054240b97b073f314321a4d0b8a2f35
Daniel Janocha <daniel.jan.janocha@gmail.com>
2023-12-03 17:33:43 +0100

    added 'gallery empty' message if no images uploaded yet

--- src/app/browse/photos/page.tsx
+++ src/app/browse/photos/page.tsx
@@ -1,12 +1,20 @@
+import { isEmpty } from "lodash";
 import { PageContainer } from "~/app/_components/page-contaiiner";
 import { PhotoGallery } from "~/app/_components/photo-gallery";
 import { api } from "~/trpc/server";
 
 export default async function BrowsePhotosPage() {
   const images = await api.files.getMyImages.query();
+  const isGalleryEmpty = isEmpty(images);
   return (
     <PageContainer title="Your photos">
-      <PhotoGallery images={images} />
+      {isGalleryEmpty ? (
+        <span className="text-wrap text-3xl font-bold text-red-100">
+          {`Gallery is still empty :)`}
+        </span>
+      ) : (
+        <PhotoGallery images={images} />
+      )}
     </PageContainer>
   );
 }
