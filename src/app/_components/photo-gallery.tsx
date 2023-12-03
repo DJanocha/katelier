b9f962265079afb3bcaeafe1c2bc0a76eb823282
Daniel Janocha <daniel.jan.janocha@gmail.com>
2023-12-03 16:45:48 +0100

    added files removal (from db, not from file hosting)

--- src/app/_components/photo-gallery.tsx
+++ src/app/_components/photo-gallery.tsx
@@ -2,21 +2,33 @@
 
 import { TrashIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-import {
-  Navigation,
-  Pagination,
-  Parallax
-} from "swiper/modules";
+import { Navigation, Pagination, Parallax } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Button } from "~/components/ui/button";
+import { useToast } from "~/components/ui/use-toast";
+import { api } from "~/trpc/react";
 import { type UploadedFile } from "~/validators/uploaded-file";
 export type PhotoGalleryProps = {
   images: UploadedFile[];
 };
 export const PhotoGallery = ({ images }: PhotoGalleryProps) => {
+  const { toast } = useToast();
+  const apiUtils = api.useUtils();
+  const router = useRouter();
+  const { mutate: deleteImage } = api.files.removeImage.useMutation({
+    onSuccess: async () => {
+      toast({ title: "Removed file" });
+      await apiUtils.files.getMyImages.invalidate();
+      router.refresh();
+    },
+    onError: () => {
+      toast({ title: "Removing file failed", variant: "destructive" });
+    },
+  });
   return (
     <Swiper
       // style={{
@@ -33,8 +45,8 @@ export const PhotoGallery = ({ images }: PhotoGalleryProps) => {
       className="mySwiper"
     >
       {images.map((image) => {
-        const title = ""//todo: extend file db model schema with optional title and use it here
-        const description = ""//todo: extend file db model schema with optional description and use it here
+        const title = ""; //todo: extend file db model schema with optional title and use it here
+        const description = ""; //todo: extend file db model schema with optional description and use it here
         return (
           <SwiperSlide key={image.id} className="overflow-hidden rounded-xl">
             <div className="relative flex h-[400px] w-[300px] flex-col justify-between ">
@@ -49,28 +61,30 @@ export const PhotoGallery = ({ images }: PhotoGalleryProps) => {
                 className="flex flex-row justify-end p-1"
                 data-swiper-parallax="-100"
               >
-                <Button variant={"ghost"} className="drop-shadow-xl shadow-green-400">
+                <Button
+                  variant={"ghost"}
+                  className="shadow-green-400 drop-shadow-xl"
+                  onClick={() => deleteImage({ id: image.id })}
+                >
                   <TrashIcon className="h-6 w-6 text-red-500" />
                 </Button>
               </div>
               <div className="flex flex-col gap-2 p-2" data-swiper-parallax="0">
                 {title ? (
-                <div
-                  className="text-slay-100 text-shadow-xl text-xl shadow-black"
-                  data-swiper-parallax="-300"
-                >
-                  {title}
-                </div>
-
+                  <div
+                    className="text-slay-100 text-shadow-xl text-xl shadow-black"
+                    data-swiper-parallax="-300"
+                  >
+                    {title}
+                  </div>
                 ) : null}
                 {description ? (
-                <div
-                  className="text-shadow-xl text-sm shadow-black"
-                  data-swiper-parallax="-300"
-                >
-                  <p>{description}</p>
-                </div>
-
+                  <div
+                    className="text-shadow-xl text-sm shadow-black"
+                    data-swiper-parallax="-300"
+                  >
+                    <p>{description}</p>
+                  </div>
                 ) : null}
               </div>
             </div>
@@ -79,5 +93,4 @@ export const PhotoGallery = ({ images }: PhotoGalleryProps) => {
       })}
     </Swiper>
   );
-
 };
--- src/server/api/routers/files-router.ts
+++ src/server/api/routers/files-router.ts
@@ -4,34 +4,70 @@ import { omit } from "lodash";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { files } from "~/server/db/schema";
-import { uploadedFileValidator, type UploadedFile, registerUploadedFileValidator } from "~/validators/uploaded-file";
+import {
+  uploadedFileValidator,
+  type UploadedFile,
+  registerUploadedFileValidator,
+} from "~/validators/uploaded-file";
 
 export const filesRouter = createTRPCRouter({
   registerImage: protectedProcedure
     .input(registerUploadedFileValidator)
     .output(uploadedFileValidator)
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(files).values({...input,ownerId: ctx.user.id});
-      const uploadedFile = await ctx.db.query.files.findFirst({ where: eq(files.key, input.key) });
-      if(!uploadedFile){
+      await ctx.db.insert(files).values({ ...input, ownerId: ctx.user.id });
+      const uploadedFile = await ctx.db.query.files.findFirst({
+        where: eq(files.key, input.key),
+      });
+      if (!uploadedFile) {
         /**
          * todo:
          * if file was uploaded to uploadthing but not registered in db, file in uploadthing should be removed
          * so that user can upload file again and there are no leftovers in uploadthing
          */
-        throw new TRPCError({code:"INTERNAL_SERVER_ERROR",message:"file was not uploaded correctly"})
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "file was not uploaded correctly",
+        });
       }
-      const uploadedFileInfo : UploadedFile = omit(uploadedFile, ['ownerId'])
+      const uploadedFileInfo: UploadedFile = omit(uploadedFile, ["ownerId"]);
       return {
         ...uploadedFileInfo,
       };
+    }),
+  removeImage: protectedProcedure
+    .input(uploadedFileValidator.pick({ id: true }))
+    .output(z.object({ removedImage: uploadedFileValidator }))
+    .mutation(async ({ ctx, input }) => {
+      const file = await ctx.db.query.files.findFirst({
+        where: eq(files.id, input.id),
+      });
+      if (!file) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "file does not exist",
+        });
+      }
+      const isMine = file.ownerId === ctx.user.id;
+      if (!isMine) {
+        throw new TRPCError({
+          code: "FORBIDDEN",
+          message: "you are not allowed to remove this file",
+        });
+      }
+      await ctx.db.delete(files).where(eq(files.id, input.id));
+      return { removedImage: file };
     }),
   getMyImages: protectedProcedure
     .input(z.void())
     .output(uploadedFileValidator.array())
     .query(async ({ ctx }) => {
-      const myImages = await ctx.db.query.files.findMany({where:eq(files.ownerId, ctx.user.id) })
-      const images : UploadedFile[] = myImages.map(image=>omit(image, ['ownerId']))
-      return uploadedFileValidator.array().parse(images)
+      const myImages = await ctx.db.query.files.findMany({
+        where: eq(files.ownerId, ctx.user.id),
+      });
+      const images: UploadedFile[] = myImages.map((image) =>
+        omit(image, ["ownerId"]),
+      );
+      return uploadedFileValidator.array().parse(images);
     }),
 });
