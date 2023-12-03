6df100fe5f44251e28e593c8b45570bdb523460b
Daniel Janocha <daniel.jan.janocha@gmail.com>
2023-12-03 15:05:04 +0100

    added separator to browse page

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -4,7 +4,7 @@ import { TileLink } from "../_components/tile-link";
 
 export default function BrowsePage() {
   return (
-    <PageContainer title="browse your stuff" description="(images, notes etc)">
+    <PageContainer title="browse your stuff" description="(images, notes etc)" showSeparator>
       <div className="flex h-full flex-1 flex-col justify-end">
         <div className="grid w-full grid-cols-2 gap-4 py-8 sm:grid-cols-2 md:gap-8">
           <TileLink
