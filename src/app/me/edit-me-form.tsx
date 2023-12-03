77c3031e5d1efd70376aa816bd6b38d7b313cde3
Daniel Janocha <daniel.jan.janocha@gmail.com>
2023-12-03 22:31:17 +0100

    added more generic component to serve as label/formInput

--- src/app/me/edit-me-form.tsx
+++ src/app/me/edit-me-form.tsx
@@ -1,26 +1,18 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { CheckIcon, EditIcon, UndoIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 import { useForm } from "react-hook-form";
-import { Badge } from "~/components/ui/badge";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+  Form
 } from "~/components/ui/form";
-import { Input } from "~/components/ui/input";
 import { useToast } from "~/components/ui/use-toast";
 import { api } from "~/trpc/react";
-import { objectKeys } from "~/utils/objectKeys";
 import { updateMeValidator, type UpdateMe } from "~/validators/update-me";
+import { EditableLabel } from "./editable-input";
 
 type UpdateMeKey = keyof UpdateMe;
+
 
 export const EditMeForm = () => {
   const { toast } = useToast();
@@ -39,149 +31,56 @@ export const EditMeForm = () => {
   const editMeForm = useForm<UpdateMe>({
     resolver: zodResolver(updateMeValidator),
   });
-  const [editableFields, setEditableFields] = useState<UpdateMeKey[]>([])
-  const makeFieldEditable = useCallback((key: UpdateMeKey) => {
-    if (editableFields.includes(key)) { return }
-    setEditableFields(prev => [...prev, key])
-  }, [editableFields])
 
   const saveField = useCallback(async (key: UpdateMeKey) => {
-    if (!editableFields.includes(key)) { return }
     const fieldValidationResult = updateMeValidator.shape[key].safeParse(editMeForm.getValues()[key])
     if (!fieldValidationResult.success) {
       await editMeForm.trigger(key)
       return
     }
-    setEditableFields(prev => prev.filter(k => k !== key))
     updateMe({ [key]: fieldValidationResult.data })
-  }, [editMeForm, editableFields, updateMe])
+  }, [editMeForm, updateMe])
 
-  const resetField = useCallback((key: UpdateMeKey) => {
-    if (!editableFields.includes(key)) { return }
-    editMeForm.setValue(key, data?.me[key])
-    editMeForm.clearErrors(key)
-    setEditableFields(prev => prev.filter(k => k !== key))
-  }, [data?.me, editMeForm, editableFields])
-
-  const updateFormValues = useCallback(
-    (vals: unknown) => {
-      const updatableFieldsValidationResult = updateMeValidator.safeParse(vals);
-      if (!updatableFieldsValidationResult.success) {
-        return;
-      }
-      const newMe = updatableFieldsValidationResult.data;
-      objectKeys(newMe).forEach((key) => {
-        editMeForm.setValue(key, newMe[key]);
-      });
-    },
-    [editMeForm],
-  );
-
-  useEffect(() => {
-    try {
-      const newMe = updateMeValidator.parse(data?.me);
-      objectKeys(newMe).forEach((key) => {
-        editMeForm.setValue(key, newMe[key]);
-      });
-    } catch (error) { }
-    updateFormValues(data?.me);
-  }, [data?.me, editMeForm, updateFormValues]);
 
   return (
     <Form {...editMeForm}>
       <form
         className="space-y-8 pt-8"
       >
-        <FormField
-          control={editMeForm.control}
+        <EditableLabel
+          form={editMeForm}
+          label="Name"
           name="name"
-          render={({ field }) => {
-            if (!editableFields.includes(field.name)) {
-              return <div className="w-full flex flex-row gap-2">
-                <Badge variant="secondary">{field.name}</Badge>
-                <span className="w-full">{data?.me?.[field.name]}</span>
-                <EditIcon className="w-8 h-8" onClick={() => makeFieldEditable(field.name)} />
-              </div>
-            }
-            return (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <div className="flex flex-row items-center space-x-4">
-                    <Input placeholder="Your name" {...field} />
-                    <CheckIcon onClick={() => saveField(field.name)} />
-                    <UndoIcon onClick={() => resetField(field.name)} />
-                  </div>
-                </FormControl>
-                <FormDescription>
-                  This is the name that will be displayed on your profile and in
-                  emails.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )
-          }}
-        />
-        <FormField
+          onSave={() => saveField("name")}
           control={editMeForm.control}
+          defaultValue={data?.me?.name}
+          description="This is the name that will be displayed on your profile and in emails."
+          key={"name"}
+          placeholder="Your name"
+        />
+        <EditableLabel
+          form={editMeForm}
+          label="Email"
           name="email"
-          render={({ field }) => {
-            if (!editableFields.includes(field.name)) {
-              return <div className="w-full flex flex-row gap-2">
-                <Badge variant="secondary">{field.name}</Badge>
-                <span className="w-full">{data?.me?.[field.name]}</span>
-                <EditIcon className="w-8 h-8" onClick={() => makeFieldEditable(field.name)} />
-              </div>
-            }
-            return (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <div className="flex flex-row items-center space-x-4">
-                    <Input placeholder="Your email" type="email" {...field} />
-                    <CheckIcon onClick={() => saveField(field.name)} />
-                    <UndoIcon onClick={() => resetField(field.name)} />
-                  </div>
-                </FormControl>
-                <FormDescription>
-                  This is the email that will be associated with your profile.
-                  Currently it won&apos;t be used for anything.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )
-          }}
-        />
-        <FormField
+          onSave={() => saveField("email")}
           control={editMeForm.control}
-          name="phoneNumber"
-          render={({ field }) => {
-            if (!editableFields.includes(field.name)) {
-              return <div className="w-full flex flex-row gap-2">
-                <Badge variant="secondary">{field.name}</Badge>
-                <span className="w-full">{data?.me?.[field.name]}</span>
-                <EditIcon className="w-8 h-8" onClick={() => makeFieldEditable(field.name)} />
-              </div>
-            }
-            return (
-              <FormItem>
-                <FormLabel>Phone number</FormLabel>
-                <FormControl>
-                  <div className="flex flex-row items-center space-x-4">
-                    <Input placeholder="Your phone numberemail" {...field} />
-                    <CheckIcon onClick={() => saveField(field.name)} />
-                    <UndoIcon onClick={() => resetField(field.name)} />
-                  </div>
-                </FormControl>
-                <FormDescription>
-                  This is the phone number that will be associated with your
-                  profile. Currently it won&apos;t be used for anything.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )
-          }}
+          defaultValue={data?.me?.email}
+          description="This is the email that will be associated with your profile. Currently it won't be used for anything."
+          key={"email"}
+          placeholder="Your email"
         />
+        <EditableLabel
+          form={editMeForm}
+          label="Phone"
+          name="phoneNumber"
+          onSave={() => saveField("phoneNumber")}
+          control={editMeForm.control}
+          defaultValue={data?.me?.phoneNumber}
+          description="This is the phone number that will be associated with your profile. Currently it won't be used for anything."
+          key={"phoneNumber"}
+          placeholder="Your phone number"
+        />
+
       </form>
     </Form>
   );
---
+++ src/app/me/editable-input.tsx
@@ -0,0 +1,59 @@
+import { CheckIcon, EditIcon, UndoIcon } from 'lucide-react';
+import { useCallback, useState } from 'react';
+import { type ControllerProps, type FieldPath, type FieldValues, type UseFormReturn } from 'react-hook-form';
+import { Badge } from '~/components/ui/badge';
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '~/components/ui/form';
+import { Input } from '~/components/ui/input';
+import { type TypedOmit } from '~/types/helpers';
+
+export const EditableLabel = <
+    TFieldValues extends FieldValues = FieldValues,
+    TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+    TForm extends UseFormReturn<TFieldValues> = UseFormReturn<TFieldValues>
+>({
+    ...props
+}: TypedOmit<ControllerProps<TFieldValues, TName>, "render"> & {
+    form: TForm;
+    label: string,
+    description?: string
+    placeholder?: string
+    onSave: () => void
+}) => {
+    const [isEditable, setIsEditable] = useState(false)
+    const resetField = useCallback(() => {
+        setIsEditable(false)
+        props.form.resetField(props.name)
+    }, [props.form, props.name])
+
+    return (
+        <FormField
+            control={props.control}
+            name={props.name}
+            render={({ field }) => {
+                if (!isEditable) {
+                    return <div className="w-full flex flex-row gap-2">
+                        <Badge variant="secondary">{props.label}</Badge>
+                        <span className="w-full">{props.defaultValue}</span>
+                        <EditIcon className="w-8 h-8" onClick={() => setIsEditable(true)} />
+                    </div>
+                }
+                return (
+                    <FormItem>
+                        <FormLabel>{props.label}</FormLabel>
+                        <FormControl>
+                            <div className="flex flex-row items-center space-x-4">
+                                <Input placeholder={props.placeholder ?? "Type here..."}{...field} />
+                                <CheckIcon onClick={props.onSave} />
+                                <UndoIcon onClick={resetField} />
+                            </div>
+                        </FormControl>
+                        <FormDescription>
+                            {props.description}
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )
+            }}
+        />
+    );
+};
