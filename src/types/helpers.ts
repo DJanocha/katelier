fafe0699b497e08b7c4cc6bc72942da1095dd303
Daniel Janocha <daniel.jan.janocha@gmail.com>
2023-12-06 22:36:33 +0100

    addded prettify utility type

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,1 +1,13 @@
 export type TypedOmit<TObj, TKey extends keyof TObj> = Omit<TObj, TKey>;
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+  // eslint-disable-next-line @typescript-eslint/ban-types
+} & {};
+
+export type DeepPrettify<T> = {
+  [K in keyof T]: T[K] extends object
+    ? DeepPrettify<T[K]>
+    : T extends object
+    ? Prettify<T[K]>
+    : T[K];
+};
