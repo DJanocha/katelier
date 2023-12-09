e14308bb1a007e059a8c9ddb4009c24e154ebfb4
Daniel Janocha <daniel.jan.janocha@gmail.com>
2023-12-09 19:35:35 +0100

    removed bg from bottom-navigation, added more distinguishing between focused, active and rest of bottom-navigation-links

--- src/app/_components/bottom-navigation-link.tsx
+++ src/app/_components/bottom-navigation-link.tsx
@@ -4,7 +4,7 @@ import { type LucideIcon } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 import { useMemo } from "react";
 import { Button } from "~/components/ui/button";
-import { type AppPathname } from "~/constants/app-pathnames";
+import { AppPathnames, type AppPathname } from "~/constants/app-pathnames";
 import { cn } from "~/lib/utils";
 
 export type BottomNavigationLinkProps = {
@@ -17,18 +17,19 @@ export const BottomNavigationLink = ({
 }: BottomNavigationLinkProps) => {
   const pathname = usePathname();
   const router = useRouter();
-  const isActive = useMemo(() => pathname === path, [path, pathname]);
+  const isActive = useMemo(() => pathname.startsWith(path), [path, pathname]);
   return (
     <Button
       key={path}
-      className="rounded-full bg-transparent "
+      className={cn(" opacity-30 focus-within:opacity-100 p-1 flex-1  outline-none focus-visible:ring-transparent backdrop-blur-lg  focus-visible:stroke-blue-400 focus-visible:ring-0 focus-visible:outline-none focus-visible:ring-offset-0",
+        isActive && 'scale-125 opacity-100')}
       size={"icon"}
       onClick={() => router.push(path)}
     >
       <Icon
-        className={cn(isActive ? "stroke-orange-500" : "stroke-white")}
-        size={40}
+        className={cn('bg-transparent h-10 w-10  ', isActive ? "stroke-orange-500" : "stroke-white")}
+        style={{ outline: 'none' }}
       />
-    </Button>
+    </Button >
   );
 };
--- src/app/_components/bottom-navigation.tsx
+++ src/app/_components/bottom-navigation.tsx
@@ -32,7 +32,7 @@ export const BottomNavigation = () => {
     return null;
   }
   return (
-    <div className="flex  w-full flex-row items-center justify-between self-stretch bg-gradient-to-b from-white/10 to-white/20 px-6 py-2">
+    <div className="flex gap-2 py-4  w-full flex-row items-baseline justify-between self-stretch px-6 ">
       {navigationLinkConfigs.map((config) => (
         <BottomNavigationLink {...config} key={config.path} />
       ))}
